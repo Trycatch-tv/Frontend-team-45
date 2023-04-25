719daca502815020152dbcb9f548e5a3c3105810
Mario Pesca <ing.mpesca@gmail.com>
2023-04-24 21:30:34 -0500

    style import fix

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import "./custom.scss";
+import "./assets/styles/custom.scss";
 
 function App() {
   return (
--- inventory/src/index.js
+++ inventory/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./custom.scss";
+import "./assets/styles/custom.scss";
 import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
